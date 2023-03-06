c00100c77f2ace8bb201d2e08b0db413067ca3d2
Mirzayev Abdulloh <abdullohmirzayev7570@gmail.com>
2023-03-06 22:41:07 +0500

    added new logo and name title

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -10,10 +10,11 @@ const Navbar = () => {
 			direction={'row'}
 			alignItems={'center'}
 			justifyContent={'space-between'}
+			p={2}
 			sx={{ position: 'sticky', top: 0, zIndex: 999, background: colors.primary }}
 		>
 			<Link to={'/'}>
-				<img src={Logo} alt='logo' height={120} />
+				<img src={Logo} alt='logo' width={'150rem'} />
 			</Link>
 			<SearchBar />
 			<Box />
